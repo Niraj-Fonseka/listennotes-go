216733dfc90c892c490b5cb15023b1f60e234e90
NirajFonseka <fonseka.live@utexas.edu>
2019-09-01 16:31:22 -0500

    fist example

--- examples/bestPodcasts.go
+++ examples/bestPodcasts.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"os"
 
+	listennotes "github.com/Niraj-Fonseka/listennotes-go"
+)
+
+func main() {
+	client := listennotes.NewListenNotesClient(os.Getenv("ListenNotesToken"))
+
+	client.BestPodcasts("", "", 0, 0)
 }
